de82a12098f9351699e6dbfac1695dbe3aaec9da
DanGarcia963 <de.espejelg@gmail.com>
2026-04-14 21:14:28 -0600

    Indications Routes

--- ARCY-imports/rutas.js
+++ ARCY-imports/rutas.js
@@ -1,25 +1,3 @@
-export const rutaInmediata = (directionsService, directionsRenderer, coordenadasActuales, mapa) => {
-  const placeHTML = document.getElementById("header-name")
-
-  const coordenadasDestino = {
-    lat: parseFloat(placeHTML.dataset.latitud),
-    lng: parseFloat(placeHTML.dataset.longitud)
-  }
-
-  directionsService
-  .route({
-    origin: coordenadasActuales,
-    destination: coordenadasDestino,
-    travelMode: google.maps.TravelMode["DRIVING"]
-  })
-  .then((response) => {
-    console.log(directionsRenderer)
-    directionsRenderer.setMap(mapa);
-    directionsRenderer.setDirections(response);
-  }).catch((e) => window.alert("Directions request failed due to " + e));
-} 
-
-
 export const rutaIti = (directionsService, directionsRenderer, coordenadasActuales, mapa, travelMode) => {
   const placeHTML = document.getElementById("nombreLugar");
 
@@ -35,8 +13,20 @@ export const rutaIti = (directionsService, directionsRenderer, coordenadasActual
       travelMode: google.maps.TravelMode[travelMode]
     })
     .then((response) => {
-      console.log(directionsRenderer);
       directionsRenderer.setMap(mapa);
       directionsRenderer.setDirections(response);
-    }).catch((e) => window.alert("Directions request failed due to " + e));
+      directionsRenderer.setPanel(document.getElementById("panelIndicaciones"));
+
+      const steps = response.routes[0].legs[0].steps;
+
+      const instrucciones = steps.map((step, index) => ({
+        paso: index + 1,
+        instruccion: step.instructions,
+        distancia: step.distance.text,
+        duracion: step.duration.text
+      }));
+
+      console.log("Indicaciones:", instrucciones);
+    })
+    .catch((e) => window.alert("Directions request failed due to " + e));
 };
